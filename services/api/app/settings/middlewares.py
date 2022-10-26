90d2ee7269ea0ac0122a815278635c9000b168b4
copper-boy <sermed512@gmail.com>
2022-10-26 21:04:30 +0300

    Update my-game project

--- services/api/app/settings/middlewares.py
+++ services/api/app/settings/middlewares.py
@@ -1,59 +1,16 @@
 from typing import Callable
 
-from aiohttp.client import ClientSession
-from aiohttp.client_exceptions import ClientResponseError
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse, Response
 
-from app.settings.config import get_auth_site_settings
-
-
-async def check_admin_authorization_token(token: str) -> bool:
-    try:
-        async with ClientSession(base_url=get_auth_site_settings().AUTH_SITE_BASE_URL) as session:
-            async with session.get(url='/api/v1/auth/admin',
-                                   params={
-                                       'token': token
-                                   },
-                                   raise_for_status=True):
-                return True
-    except ClientResponseError:
-        return False
-
-
-async def check_admin_authorization_header(authorization: str) -> bool:
-    try:
-        async with ClientSession(base_url=get_auth_site_settings().AUTH_SITE_BASE_URL,
-                                 headers={'authorization': authorization}) as session:
-            async with session.get(url='/api/v1/auth/current',
-                                   raise_for_status=True) as response:
-                json = await response.json()
-    except (ClientResponseError, TypeError):
-        return False
-
-    return json['detail']['user']['is_admin']
+from app.settings.config import get_admin_settings
 
 
 async def admin_middleware(request: Request, call_next: Callable) -> Response:
-    if request.url.path in {
-        '/api/openapi.json',
-        '/api/docs',
-        '/api/redoc',
-        '/favicon.ico'
-    }:
-        response = await call_next(request)
-        return response
-
     token = request.query_params.get('token', None)
-    header = request.headers.get('authorization', None)
 
-    if token:
-        is_admin = await check_admin_authorization_token(token)
-    else:
-        is_admin = await check_admin_authorization_header(header)
-
-    if not is_admin:
+    if token != get_admin_settings().INFINITY_ADMIN_TOKEN:
         return JSONResponse(status_code=403,
                             content={
                                 'detail': 'user is not admin'
